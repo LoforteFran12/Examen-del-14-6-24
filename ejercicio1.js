9563a75262b179126bc4607c982a955a9113f1d4
LoforteFran12 <d480670@alumnos.itr.edu.ar>
2024-08-30 09:16:51 -0300

    cambio algo

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,8 +2,8 @@
 const numeros = [5, 9, 25, 34, 40, 46, 70, 75, 88, 65, 10];
 console.log('valores iniciales ' + numeros);
 
-// en este caso "a" es 4 y "b" 2
-let isdivisible = numeros.reduce(n => {return n % 4 === 0, n % 2 === 0});
+// en este caso "a" es 15 y "b" 10
+let isdivisible = numeros.reduce(n => {return n % 15 === 0, n % 10 === 0});
 console.log(isdivisible);
 
 //filtro todos los numeros divisibles por cinco y que me lo devuelva en la consola
